3a2bf01f215f8bc7c67e136ba934b7ebb1cb2eaf
BronwenQA <bronwenmeijer@gmail.com>
2015-09-27 10:34:21 +0200

    Test to check Twitter's language support.

---
+++ TwitterLanguagesTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+This TestCase checks that Twitter supports French as a language, and not Afrikaans.
+*/
+
+class TwitterLanguagesTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl('https://twitter.com/');
+    }
+
+    protected function screenshot($file) 
+    {
+        $filedata = $this->currentScreenshot();
+        file_put_contents($file, $filedata);
+    }
+
+    protected function findLanguage($language)
+    {
+	$this->url('https://twitter.com/');
+	$element = $this->byCssSelector('b.caret');
+	$element->click();
+	try {
+		$element = $this->byLinkText("$language");
+		$element->click();
+		return true;
+	} catch(exception $e) {
+		return false;
+	} finally {
+		$this->screenshot( __DIR__.'/'.$this->getName().'-'.time(). '.png');
+    	}
+    }
+
+    public function testFrench()
+    {
+	$this->assertTrue($this->findLanguage("Français"));
+    }
+
+    public function testAfrikaans()
+    {
+	$this->assertFalse($this->findLanguage("Afrikaans"));
+    }
+
+}
+?>
+
